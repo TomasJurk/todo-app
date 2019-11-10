01979d516f62f35d593779ff6f90d20464ee2e9a
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2019-11-10 22:35:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+let toDoApp = {
+    tasks: [],
+    newTask: document.getElementById('newTaskInput'),
+    tasksContainer: document.querySelector('.task-list'),
+    displayedTasks: document.getElementsByClassName('task'),
+    addTask() {
+        if(this.newTask.value.length < 1) {
+            return;
+        }
+        this.tasks.push({
+            taskTekst: this.newTask.value,
+            taskDone: false
+        });
+        this.saveToLocalStorage();
+        this.showTasks();
+        this.newTask.value = '';
+    },
+    saveToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
+    markAsDone(taskID) {
+        let task = this.tasks[taskID];
+        task.taskDone = !task.taskDone;
+        if(task.taskDone) {
+            this.displayedTasks[taskID].style.textDecoration = 'line-through';
+        } else {
+            this.displayedTasks[taskID].style.textDecoration = 'none';
+        }
+        this.saveToLocalStorage();
+        
+    },
+    getSavedTasks() {
+        if(localStorage.getItem('tasks')) {
+            this.tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+        return;
+        
+    },
+    showTasks() {
+        this.tasksContainer.innerHTML = '';
+        for (let i = 0; i < this.tasks.length; i++) {
+            this.tasksContainer.innerHTML += `
+                <div class="task">
+                    <p onclick="toDoApp.markAsDone(${i})">${this.tasks[i].taskTekst}</p>
+                    <button class="task-remove-btn" onclick="toDoApp.deleteTask(${i})"></button>
+                </div>
+            `;            
+        }
+    },
+    deleteTask(taskID) {
+        // ......
+        this.saveToLocalStorage();
+        this.showTasks();
+    }
+};
+
+toDoApp.getSavedTasks();
+toDoApp.showTasks();
